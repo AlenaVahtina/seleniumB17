f313f13175dcc0c50e30cedadf15d880e32d3ba4
alena <avakhtina@phoenixit.ru>
2020-08-06 14:21:53 +0300

    L3 E5

---
+++ seleniumL2/src/test/java/UseFFOld.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class UseFFOld {
+    private WebDriver driver;
+
+    @Before
+    public void start(){
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE, false);
+        driver = new FirefoxDriver(caps);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
