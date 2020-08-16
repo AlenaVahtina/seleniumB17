1e40ae8bf8e2397c68dab2fd21830ff6f1cbd3da
alena <avakhtina@phoenixit.ru>
2020-08-17 00:23:45 +0300

    E11L6

--- seleniumL2/src/test/java/CreateUser.java
+++ seleniumL2/src/test/java/CreateUser.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static settings.IdentifyOS.isUnix;
 import static settings.IdentifyOS.isWindows;
@@ -28,7 +28,7 @@ public class CreateUser {
 
 
     @Test
-    public void stickerTest() {
+    public void createUser() {
         driver.get("http://localhost/litecart/en/");
         //Создание нового пользователя
         driver.findElement(By.xpath("//a[.='New customers click here']")).click();
@@ -37,25 +37,31 @@ public class CreateUser {
         driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Vimes");
         driver.findElement(By.xpath("//input[@name='address1']")).sendKeys("Ankh-Morkpork");
         driver.findElement(By.xpath("//input[@name='address1']")).sendKeys("Guard Headquarters");
-        driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys("1111 1111");
+        driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys("10100");
         driver.findElement(By.xpath("//input[@name='city']")).sendKeys("Ankh-Morkpork");
         driver.findElement(By.xpath("//b[@role='presentation']")).click();
         driver.findElement(By.xpath("//li[.='United States']")).click();
         String mail;
         Random random = new Random();
         mail="Ank"+random.nextInt()+"@mail.com";
+        System.out.println(mail);
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys(mail);
         driver.findElement(By.xpath("//input[@name='phone']")).sendKeys("0000");
         driver.findElement(By.xpath("//input[@name='password']")).clear();
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("test1234");
-        driver.findElement(By.xpath("//input[@name='confirmed_password']")).sendKeys("test1234");
+        String password = "test1234";
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@name='confirmed_password']")).sendKeys(password);
         driver.findElement(By.xpath("//button[@name='create_account']")).click();
-
-
-
-
-
-
+        //Выход из только что созданной записи
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[.='Logout']")).click();
+        //Вход
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys(mail);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//button[@name='login']")).click();
+        //Выход
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//a[.='Logout']")).click();
     }
 
 
